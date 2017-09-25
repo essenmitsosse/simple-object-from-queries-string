3d95ba0512fcdd0139f8c14adf7ae23bc0f8b1db
marcus <marcus@essenmitsosse.de>
2017-09-25 18:56:10 +0200

    Fix critical implementation bug

--- simple-object-from-queries-string.js
+++ simple-object-from-queries-string.js
@@ -1,8 +1,8 @@
 "use strict";
 
 module.exports = function () {
-	var urlQueriesString = require( "./lib/getUrlQueriesString" ),
+	var getUrlQueriesString = require( "./lib/getUrlQueriesString" ),
 		convertStringToObject = require( "./lib/convertStringToObject" );
 
-	return convertStringToObject( urlQueriesString );
+	return convertStringToObject( getUrlQueriesString() );
 };
